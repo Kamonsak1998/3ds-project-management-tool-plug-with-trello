5af654fe1355e464daf64f326da98fd7d2bc9efb
WindBWing <arram-s@rmutp.ac.th>
2019-08-01 15:21:37 +0700

    update develop
    delete folder vue.js move file outside

--- go/schema/002dataMember.sql
+++
@@ -1,9 +0,0 @@
-/*
--- Query: SELECT * FROM 3DSProjectTello.001member
-LIMIT 0, 1000
-
--- Date: 2019-07-29 00:43
-*/
-INSERT INTO `` (`id`,`email`,`password`,`firstname`,`lastname`,`phone`) VALUES (1,'yinglae@gmaiil.com','AAA1234','Beam','NTP','0123456789');
-INSERT INTO `` (`id`,`email`,`password`,`firstname`,`lastname`,`phone`) VALUES (2,'aaa@aaa.com','AAA','AAA','AAA','AAA');
-INSERT INTO `` (`id`,`email`,`password`,`firstname`,`lastname`,`phone`) VALUES (3,'bbb@bbb.com','BBB','BBB','BBB','BBB');
