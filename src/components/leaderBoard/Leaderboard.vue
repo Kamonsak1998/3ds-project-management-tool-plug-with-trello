ccb1302a9c68cac9b8c6d779a9dfec44726e05f3
BeNz <wuttichai-n@rmutp.ac.th>
2019-08-02 11:36:01 +0700

    update leaderboard

--- src/components/leaderBoard/Leaderboard.vue
+++ src/components/leaderBoard/Leaderboard.vue
@@ -1,51 +1,89 @@
 <template>
-  <div class="container">
-    <p class="p">Test</p>
-    <b-table
-      class="table"
-      :items="items"
-      :fields="fields"
-      :sort-by.sync="sortBy"
-      :sort-desc.sync="sortDesc"
-      responsive="sm"
-    ></b-table>
-  </div>
+  <b-container class="bv-example-row mt-5">
+    <!-- <b-row align-v="center" class="hight">
+      <b-col>Rank</b-col>
+      <b-col cols="5">Name</b-col>
+      <b-col>score</b-col>
+    </b-row>
+    <b-row v-for="item in item" :key="item">
+      <b-col>1</b-col>
+      <b-col>2</b-col>
+      <b-col>3</b-col>
+    </b-row> -->
+
+    <b-row>
+      <b-table col-lg-5 striped hover outlined :items="items"></b-table>
+    </b-row>
+
+    <div class="col-lg-10 mx-auto">
+      <div class="card">
+        <div class="card-header">
+          <i class="fa fa-align-justify"></i> Sprint 1 
+        </div>
+        <div class="card-body">
+          <table class="table table-responsive-sm table-striped">
+            <thead>
+              <tr>
+                <th>Rank</th>
+                <th>Name</th>
+                <th>Score</th>
+              </tr>
+            </thead>
+            <tbody>
+              
+            </tbody>
+          </table>
+          <ul class="pagination">
+            <li class="page-item">
+              <a class="page-link" href="#">Prev</a>
+            </li>
+            <li class="page-item active">
+              <a class="page-link" href="#">1</a>
+            </li>
+            <li class="page-item">
+              <a class="page-link" href="#">2</a>
+            </li>
+            <li class="page-item">
+              <a class="page-link" href="#">3</a>
+            </li>
+            <li class="page-item">
+              <a class="page-link" href="#">4</a>
+            </li>
+            <li class="page-item">
+              <a class="page-link" href="#">Next</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </b-container>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      sortBy: "score",
-      sortDesc: false,
-      fields: [
-      
-        { key: "isActive", sortable: false },
-        { key: "last_name", sortable: true },
-        { key: "first_name", sortable: true },
-        { key: "age", sortable: true }
-      ],
-        items: [
-          { isActive: true, age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
-          { isActive: false, age: 21, first_name: 'Larsen', last_name: 'Shaw' },
-          { isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson' },
-          { isActive: true, age: 38, first_name: 'Jami', last_name: 'Carney' }
-        ]
+      items: [
+        { age: 40, first_name: "Dickerson", last_name: "Macdonald" },
+        { age: 21, first_name: "Larsen", last_name: "Shaw" },
+        { age: 89, first_name: "Geneva", last_name: "Wilson" },
+        { age: 38, first_name: "Jami", last_name: "Carney" }
+      ]
     };
   }
 };
 </script>
 
 <style>
-.p {
-  margin-top: 50px;
-}
-.table {
-  text-align: center;
-}
-.table thead th {
+/* .table thead th {
   vertical-align: bottom;
   border-bottom: 2px solid #dee2e6;
   text-align: center;
+} */
+.hight {
+  min-height: 4rem;
+  margin-top: 50px;
+
+  font-size: 20px;
 }
 </style>
