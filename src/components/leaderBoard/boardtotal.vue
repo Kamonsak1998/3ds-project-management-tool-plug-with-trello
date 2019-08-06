6c782f09946e0c0fb7274523696d9206acf98d77
BeNz <wuttichai-n@rmutp.ac.th>
2019-08-06 15:23:55 +0700

    update table code

--- src/components/leaderBoard/boardtotal.vue
+++ src/components/leaderBoard/boardtotal.vue
@@ -79,22 +79,6 @@ a:hover {
     margin: 0 auto;
   }
 
-  .container-table100 {
-    width: 100%;
-    min-height: 100vh;
-    background: #d1d1d1;
-
-    display: -webkit-box;
-    display: -webkit-flex;
-    display: -moz-box;
-    display: -ms-flexbox;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-wrap: wrap;
-    padding: 33px 30px;
-  }
-
   .wrap-table100 {
     width: 1300px;
   }
