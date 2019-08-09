c5492ce1960d36a8b180b869bf1572f884499cd6
BeNz <wuttichai-n@rmutp.ac.th>
2019-08-09 18:22:17 +0700

    update board

--- src/components/leaderBoard/Leaderboard.vue
+++ src/components/leaderBoard/Leaderboard.vue
@@ -1,14 +1,18 @@
 <template>
       <div class="container">
         <boardtotal></boardtotal>
+         <allboard></allboard>
       </div>
 </template>
 
 <script>
 import boardtotal from '@/components/leaderBoard/boardtotal.vue'
+import allboard from '@/components/leaderBoard/allboard.vue'
+
 export default {
   components: {
-    boardtotal
+    boardtotal,
+    allboard,
   }
 }
 </script>
---
+++ src/components/leaderBoard/allboard.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container">
+    <div class="w3-container">
+      <h2>Slideshow Indicators</h2>
+      <p>An example of using buttons to indicate how many slides there are in the slideshow, and which slide the user is currently viewing.</p>
+    </div>
+
+    <div class="w3-content" style="max-width:800px">
+      <img class="mySlides" src="img_nature_wide.jpg" style="width:100%" />
+      <img class="mySlides" src="img_snow_wide.jpg" style="width:100%" />
+      <img class="mySlides" src="img_mountains_wide.jpg" style="width:100%" />
+    </div>
+
+    <div class="w3-center">
+      <div class="w3-section">
+        <button class="w3-button w3-light-grey" onclick="plusDivs(-1)">❮ Prev</button>
+        <button class="w3-button w3-light-grey" onclick="plusDivs(1)">Next ❯</button>
+      </div>
+      <button class="w3-button demo" onclick="currentDiv(1)">1</button>
+      <button class="w3-button demo" onclick="currentDiv(2)">2</button>
+      <button class="w3-button demo" onclick="currentDiv(3)">3</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    date:{
+       
+    },
+    methods:{
+        
+
+    }
+
+}
+</script>
+
+<style>
+</style>
