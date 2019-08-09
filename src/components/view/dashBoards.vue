f5b4cb66851f339d8ec86672e31b34fd107a26fd
BeNz <wuttichai-n@rmutp.ac.th>
2019-08-09 18:31:28 +0700

    update dashboard and rebuild code

--- src/components/view/dashBoards.vue
+++ src/components/view/dashBoards.vue
@@ -4,7 +4,6 @@
       <div class="row">
         <div v-for="(result,i) in results" :key="i" class="col-sm-4">
           <div class="card-dash p-4 shadow bg-white text-center">
-            <!-- {{ result.title }} -->
             <div class="card-body">
               <p class="card-text"  v-html="result.prevDesc"></p>
                <!-- <router-link :to="{name : 'leaderboard'}">
@@ -50,23 +49,9 @@ export default {
       ]
     };
   },
-  mounted: {},
+  
   methods: {
-    auth() {
-      axios.get("http://localhost:9000/getboard").then(res => {
-        console.log(res);
-        // const token = res.data.acctoken;
-        // const stat = res.data.accstat;
-        if (stat == true) {
-          // this.$store.commit("setToken", token);
-          // this.$router.push("/dashBoards");
-        }
-      });
-    }
-
-    // login:function(){
-    //     this.$router.push("/login");
-    // }
+   
   }
 };
 </script>
@@ -80,11 +65,8 @@ export default {
 }
 .card-dash{
   margin-top: 15px;
-  /* white-space: nowrap;  */
+
   width: 250px; 
-  /* overflow: hidden; */
-  /* height:100%; */
-  /* overflow: hidden; text-overflow: ellipsis; -webkit-box-orient: vertical; display: -webkit-box; -webkit-line-clamp: 2; */
   border-radius: 25px;
 }
 .card-text {
