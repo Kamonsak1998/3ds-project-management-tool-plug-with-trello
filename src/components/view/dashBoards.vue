8dfe567bfdba06b165580d74fd565789bc124051
WindBWing <arram-s@rmutp.ac.th>
2019-08-13 10:18:58 +0700

    update auth
    edit path get server

--- src/components/view/dashBoards.vue
+++ src/components/view/dashBoards.vue
@@ -11,7 +11,7 @@
           </div>
         </div>
       </div>
-    </div>
+    </div> 
   </div>
 </template>
 
@@ -22,7 +22,7 @@ export default {
   mounted: function() {
     if (this.token != "") {
       axios
-        .post("http://ddc1cade.ngrok.io/getdashboard", { token: this.token })
+        .post("http://6454d735.ngrok.io/getdashboard", { token: this.token })
         .then(Response => {
           console.log("Db", Response);
           this.results = Response.data.board
