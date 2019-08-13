146533cbdef44270bd75b5f1aaf6ee70d70ee687
BeNz <wuttichai-n@rmutp.ac.th>
2019-08-13 17:56:37 +0700

    check idboard

--- src/components/leaderBoard/Leaderboard.vue
+++ src/components/leaderBoard/Leaderboard.vue
@@ -1,20 +1,31 @@
 <template>
-      <div class="container">
-        <boardtotal></boardtotal>
-         <!-- <allboard></allboard> -->
-      </div>
+  <div class="container">
+    <boardtotal></boardtotal>
+    <!-- <allboard></allboard> -->
+  </div>
 </template>
 
 <script>
-import boardtotal from '@/components/leaderBoard/boardtotal.vue'
-import allboard from '@/components/leaderBoard/allboard.vue'
-
+import boardtotal from "@/components/leaderBoard/boardtotal.vue";
+import allboard from "@/components/leaderBoard/allboard.vue";
+import { mapGetters } from "vuex";
 export default {
+  mounted: function() {
+    if (this.idBoard != "") {
+      return;
+    } else {
+      this.$router.push("/dashboards");
+      return;
+    }
+  },
+  computed: {
+    ...mapGetters(["idBoard"])
+  },
   components: {
     boardtotal,
-    allboard,
+    allboard
   }
-}
+};
 </script>
 
 <style>
