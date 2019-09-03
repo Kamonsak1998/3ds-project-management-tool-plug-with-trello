e01c3d15b676c542a6c40c6610e92189900c4a01
BeNz <wuttichai-n@rmutp.ac.th>
2019-09-04 01:49:58 +0700

    update config setdate

--- src/services/BoardService.js
+++ src/services/BoardService.js
@@ -1,12 +1,36 @@
-import {mainAPIOptions} from "../constant/config";
-import {BaseService} from "./BaseService";
+import { mainAPIOptions } from "../constant/config";
+import { BaseService } from "./BaseService";
 
 export class BoardService extends BaseService {
     fetchDashboard = (token) => {
-        return this.requester.post('/getdashboard', {token}, mainAPIOptions())
+        return this.requester.post('/getdashboard', { token }, mainAPIOptions())
     }
 
-    fetchHistory = ({token, idBoard}) => {
-        return this.requester.post('/gethistory', {token, idBoard}, mainAPIOptions())
+    fetchHistory = ({ token, idBoard }) => {
+        return this.requester.post('/gethistory', { token, idBoard }, mainAPIOptions())
+    }
+
+    fetchLeaderboard = ({ token, idBoard }) => {
+        return this.requester.post('/getleaderboard', { token, idBoard }, mainAPIOptions())
+    }
+
+    fetchSetdatetime = ({
+        startDate,
+        sprintDay,
+        endDate,
+        idBoard,
+        boardName
+    }) => {
+        return this.requester.post('/setdate', {
+            startDate,
+            sprintDay,
+            endDate,
+            idBoard,
+            boardName
+        }, mainAPIOptions())
+    }
+
+    fetchchecksetdate = ({idBoard}) => {
+        return this.requester.post('/checksetdate', {idBoard}, mainAPIOptions())
     }
 }
